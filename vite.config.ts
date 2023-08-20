4a9bab18fa5f7efff7c1d52a7887a300e2994228
Supermog <balint@gearedapp.co.uk>
2023-08-20 18:58:52 +0200

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      exclude: [],
+    }),
+  ],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
