3fa51b2e046a76cad9526aadc171f1961ab9356b
Supermog <balint@gearedapp.co.uk>
2024-03-16 11:20:59 +0100

    chore: wording in about section

--- src/sections/about-section.tsx
+++ src/sections/about-section.tsx
@@ -1,4 +1,3 @@
-import { aboutSection } from "../utils/strings";
 import aboutSectionPic from "@/assets/about-section-pic.png";
 
 function AboutSection() {
@@ -15,7 +14,23 @@ function AboutSection() {
               A dedicated Full-Stack developer with a focus on Front-End based
               in Veszprém, Hungary 📍
             </p>
-            <p className="text-lg text-gray-300">{aboutSection}</p>
+            <p className="text-lg text-gray-300">
+              From a young age, I've been fascinated by the endless
+              possibilities of the internet and its ability to connect people
+              around the globe. This fascination drove me to pursue a career in
+              web development, where I thrive on blending creativity with
+              cutting-edge technology to bring ideas to life. As a web
+              developer, I've honed my skills in both frontend and backend
+              development, allowing me to create seamless, user-friendly
+              interfaces while ensuring robust functionality behind the scenes.
+              Whether it's bringing a client's vision to fruition or tackling a
+              personal project, I approach each project with enthusiasm and
+              attention to detail. In addition to my technical expertise, I'm
+              deeply committed to keep up the latest industry trends and best
+              practices. I thrive in dynamic environments where I can
+              continually learn, grow and also where transparency with team
+              members is a must in communication.
+            </p>
           </div>
         </div>
       </div>
