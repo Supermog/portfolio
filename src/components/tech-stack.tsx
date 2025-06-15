939f5e308193d5b51294004071835b88cda59d52
Supermog <balint@gearedapp.co.uk>
2025-06-15 19:34:42 +0200

    chore: add widths and heights to the tech stack icons

--- src/components/tech-stack.tsx
+++ src/components/tech-stack.tsx
@@ -15,13 +15,15 @@ function TechStack() {
       <p className="text-xl lg:border-r-2 lg:pr-5 lg:pb-0 pb-5 mb-5 lg:mb-0 lg:border-b-0 border-b-2">
         Tech Stack
       </p>
-      <div className="gap-12 grid grid-rows-2 grid-flow-col lg:flex lg:items-center">
+      <div className="gap-12 grid grid-rows-2 grid-flow-col lg:flex items-center sm:justify-center">
         <img
           src={typescriptLogo}
           alt="typescript"
           className="h-14 hover:cursor-pointer"
           data-tooltip-id="technology"
           data-tooltip-content="TypeScript"
+          width="45.5"
+          height="45.5"
         />
         <img
           src={reactLogo}
@@ -29,6 +31,8 @@ function TechStack() {
           className="h-14 hover:cursor-pointer"
           data-tooltip-id="technology"
           data-tooltip-content="React"
+          width="45.5"
+          height="45.5"
         />
         <img
           src={tailwindLogo}
@@ -36,6 +40,8 @@ function TechStack() {
           className="h-9 hover:cursor-pointer"
           data-tooltip-id="technology"
           data-tooltip-content="Tailwind CSS"
+          width="45.5"
+          height="45.5"
         />
         <img
           src={nextJsLogo}
@@ -43,6 +49,8 @@ function TechStack() {
           className="h-14 hover:cursor-pointer"
           data-tooltip-id="technology"
           data-tooltip-content="NextJS"
+          width="45.5"
+          height="45.5"
         />
         <img
           src={nodeJsLogo}
@@ -50,6 +58,8 @@ function TechStack() {
           className="h-14 hover:cursor-pointer"
           data-tooltip-id="technology"
           data-tooltip-content="NodeJS"
+          width="40.29"
+          height="45.5"
         />
         <img
           src={nestJsLogo}
@@ -57,6 +67,8 @@ function TechStack() {
           className="h-14 hover:cursor-pointer"
           data-tooltip-id="technology"
           data-tooltip-content="NestJS"
+          width="45.5"
+          height="45.5"
         />
         <img
           src={firebaseLogo}
@@ -64,6 +76,8 @@ function TechStack() {
           className="h-14 hover:cursor-pointer"
           data-tooltip-id="technology"
           data-tooltip-content="Firebase"
+          width="33"
+          height="45.5"
         />
         <img
           src={mongodbLogo}
@@ -71,6 +85,8 @@ function TechStack() {
           className="h-14 hover:cursor-pointer"
           data-tooltip-id="technology"
           data-tooltip-content="MongoDB"
+          width="30"
+          height="45.5"
         />
       </div>
     </div>
